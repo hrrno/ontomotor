9c4179d1464cefcd20e4fddfc8b8e28df3f997b5
Aaron Comyn <aaron@roxor.no>
2016-01-21 12:57:51 +0100

    Type provider first steps

---
+++ Ontomotor/ProtoTypeProvider/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace ProtoTypeProvider
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("ProtoTypeProvider")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("aaron")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
