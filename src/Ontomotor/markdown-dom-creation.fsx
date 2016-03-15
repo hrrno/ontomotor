1c6dc0c04e1e438db5beed1aa0b62a05b4965514
Aaron Comyn <aaco@fhi.no>
2016-03-15 10:57:49 +0100

    Not working well, closer, but still not working

--- src/Ontomotor/markdown-dom-creation.fsx
+++ src/Ontomotor/markdown-dom-creation.fsx
@@ -19,6 +19,48 @@ Eq >> 2 >> Dateprop: 2011-10-10
 
 
 
+type Token = 
+    | Root     of position : int * content: string 
+    | Header   of position : int * content: string 
+    | Yaml     of position : int * content: string
+    | Property of position : int * content: string
+    //| Blank
+    with member x.Id = 
+            match x with
+            | Root     (i,c) -> i
+            | Header   (i,c) -> i
+            | Yaml     (i,c) -> i
+            | Property (i,c) -> i
+            //| Blank          -> -1
+         member x.Content = 
+            match x with
+            | Root     (i,c) -> c
+            | Header   (i,c) -> c
+            | Yaml     (i,c) -> c
+            | Property (i,c) -> c
+            //| Blank          -> -1
+
+
+type Comparison = | Gt | Lt | Eq
+let compare (first, second) =
+    match first with
+    | Root _ -> Gt
+    | Header _ -> 
+        match second with 
+        | Header _ -> Eq
+        | _ -> Gt
+    | Yaml _ -> 
+        match second with 
+        | Header _ -> Lt
+        | Yaml _ | Property _ -> Eq
+        | _ -> Lt
+    | Property _ ->
+        match second with 
+        | Yaml _ | Property _ -> Eq
+        | _ -> Lt
+        
+                    
+
 type Tree<'LeafData,'INodeData> =
     | LeafNode of 'LeafData
     | InternalNode of 'INodeData * Tree<'LeafData,'INodeData> seq
@@ -47,19 +89,119 @@ module Tree =
             // ... and return it
             finalAccum 
 
+    let rec map fLeaf fNode (tree:Tree<'LeafData,'INodeData>) = 
+        let recurse = map fLeaf fNode  
+        match tree with
+        | LeafNode leafInfo -> 
+            let newLeafInfo = fLeaf leafInfo
+            LeafNode newLeafInfo 
+        | InternalNode (nodeInfo,subtrees) -> 
+            let newNodeInfo = fNode nodeInfo
+            let newSubtrees = subtrees |> Seq.map recurse 
+            InternalNode (newNodeInfo, newSubtrees)
 
-type FileInfo = {name:string; fileSize:int}
-type DirectoryInfo = {name:string; dirSize:int}
+
+
+(*
+let totalSize fileSystemItem =
+    let fFile acc (file:FileInfo) = 
+        acc + file.fileSize
+    let fDir acc (dir:DirectoryInfo)= 
+        acc + dir.dirSize
+    Tree.fold fFile fDir 0 fileSystemItem 
+*)
+
+type FileInfo = {name:string; fileSize:int;Length:int}
+type DirectoryInfo = {name:string; dirSize:int; }
 
 type FileSystemItem = Tree<FileInfo,DirectoryInfo>
 
+type Section = {Title:string}
+type Property = {Name:string;Content:string }
 
-type Section = { Depth:int; Title:string }
-type Content = {Type:string;Content:string }
+type MdDom = Tree<Property,Section>
 
-type MdDom = Tree<Content,Section>
+let fromProperty (contentInfo:Property) = MdDom.LeafNode contentInfo
+let fromSection (sectionInfo:Section) subitems = MdDom.InternalNode (sectionInfo, subitems)
 
-let fromSection (sectionInfo:Section) subitems = InternalNode (sectionInfo, subitems)
-let fromSection (contentInfo:Content) = LeafNode contentInfo
+
+let h2Content = fromProperty {Name="Prop"; Content="there"}
+
+let h2 = fromSection {Title="#Bar"} [h2Content]
+let h1 = fromSection {Title="#Foo"} [h2]
+let root = fromSection {Title="Document Root"} [h1]
+
+
+let document = 
+         [Root (0,"Document Root"); Header (2,"# Foo"); Header (11,"## Bar");
+           Header (21,"## Beans"); Header (33,"## Boo"); Property (51,"Baz: There");
+           Header (65,"## SubObject"); Property (161,"Autoprop: This is an autoprop");
+           Property (194,"Boolprop: true"); Property (212,"Dateprop: 2011-10-10")]
+
+document |> List.rev
+
+//document |> foldInProperties
+
+// If youre a header and I`m a prop, I`m yours
+// If I`m a header and you`re a header, I`m yours if smaller, your parents if not
+
+let totalSize fileSystemItem =
+    let fFile acc (file:FileInfo) = 
+        acc + file.Length
+    let fDir acc (dir:DirectoryInfo)= 
+        acc 
+    Tree.fold fFile fDir 0 fileSystemItem 
+
+open System.IO
+
+
+//let fromToken token = 
+//    match token with
+//    | Root (i,c) | Header (i,c) -> fromSection { Title = c }
+//    | Yaml (i,c) | Property (i,c) -> fromProperty { Name = c; Content = c  }
+
+let propFromContent content =
+    fromProperty { Name = content; Content=content; }
+
+//
+//let rec readChildrenss (document:Token list) =
+//    let subItems = seq {
+//        for token in document do
+//            match token with
+//            | Root (i,c) | Header (i,c) -> yield! readChildren (propFromContent c)
+//            | Yaml (i,c) | Property (i,c) -> yield! fromProperty { Name = c; Content = c  }
+//        }
+//    MdDom.InternalNode ({Title="Root"}, subItems )
+
+
+
+
+let fromFile (fileInfo:FileInfo) = 
+    LeafNode fileInfo 
+
+let rec fromDir (dirInfo:DirectoryInfo) = 
+    let subItems = seq{
+        yield! dirInfo.EnumerateFiles() |> Seq.map fromFile
+        yield! dirInfo.EnumerateDirectories() |> Seq.map fromDir
+        }
+    InternalNode (dirInfo,subItems)
+
+    
+
+let rec readChildren (document:Token list) =
+    match document with
+    | [] -> MdDom.InternalNode ({Title="Root"}, [] )
+    | token::xs -> 
+        let rec subItems = seq {
+            match token with
+            | Root (i,c) | Header (i,c) -> yield! readChildren xs
+            | Yaml (i,c) | Property (i,c) -> yield! fromProperty { Name = c; Content = c  }
+        }
+        MdDom.InternalNode ({Title=token.Content}, subItems)
+            
+
+
+
+
 
 
