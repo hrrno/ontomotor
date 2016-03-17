c56bfa9c71f6522a22875e390f2fe4aefc2ea131
Aaron Comyn <aaco@fhi.no>
2016-03-17 11:46:04 +0100

    Adapting the provider example to the needs of the Ontomotor

--- src/OntomotorTest/ProviderTest/Script.fsx
+++ src/OntomotorTest/ProviderTest/Script.fsx
@@ -2,10 +2,11 @@
 
 
 // Loading test libraries
-#r @"../../Ontomotor/bin/Debug/Ontomotor.dll"
-#r @"../../ProtoTypeProvider/bin/Debug/ProtoTypeProvider.dll"
-
-
-
-
-let obj1 = new ProtoTypeProvider.ProtoTypeProvider()
+//#r @"../../Ontomotor/bin/Debug/Ontomotor.dll"
+//#r @"../../ProtoTypeProvider/bin/Debug/ProtoTypeProvider.dll"
+//
+//
+//
+//
+//let obj1 = new Proto.TypeProvider.Type4("yo") // ProtoTypeProvider.ProtoTypeProvider()
+//obj1.InstanceMethod
