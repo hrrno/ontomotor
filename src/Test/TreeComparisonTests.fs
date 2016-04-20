1af04aa64172e3240fdc1a704e1d3f90aacdcfd6
Aaron Comyn <aaco@fhi.no>
2016-04-20 10:35:30 +0200

    Creating functional module

--- src/Test/TreeComparisonTests.fs
+++ src/Test/TreeComparisonTests.fs
@@ -135,6 +135,8 @@ let t5 = [ Root(0, "Root")
 
 
 
+
+
 type MyUnion =
     | A of int * string
     | B of int * string
@@ -142,116 +144,115 @@ type MyUnion =
          member x.Name = match x with | A (i,n) -> n | _ -> "none"
 
 
-type IItem = { Name : string; }
-
-type ITree = 
-    | IFace of IItem * ITree list
-    | IProp of IItem
-    with member x.Item = match x with | IFace(n,sub) -> n | IProp (n) -> n
-         member x.Sub  = match x with | IFace(n,sub) -> sub | IProp _ -> []
-         member x.Props = x.Sub |> List.filter (function | IProp _ -> true | _ -> false)
-         member x.Faces = x.Sub |> List.filter (function | IFace _ -> true | _ -> false)
-
-let rec findProps (Node(token, subTokens):TokenTree) : ITree =
-    match token with 
-        | Header (i,c) | Root (i,c) -> 
-            let item = { Name  = "I" + token.Title; }
-            let sub  = [ for i in subTokens do yield findProps i ]
-            IFace(item, sub)     
-        | Property (i,c) | Yaml (i,c) -> 
-            IProp({ Name = token.Title })
-
-let rec printProps (tree:ITree) =
-    printfn "interface: %s = %s" tree.Item.Name tree.Item.Name
-    match tree with
-    | IProp _ -> ()
-    | IFace (item, sub) -> for s in sub do printProps s
-
-                 
-let props item = match item with | IProp _ -> true | _ -> false
-let ifaces item = match item with | IFace _ -> true | _ -> false
-let justProps item = (item:ITree).Sub |> List.filter props 
-let justIFaces item = (item:ITree).Sub |> List.filter ifaces 
-let withSubtree s = !(s:Set<ITree> ref) |> Set.filter (fun item -> not (item.Sub.IsEmpty))
-let removeFrom s item = (s:Set<ITree> ref) := (!s).Remove item
-let addTo s item = (!(s:Set<ITree> ref)).Add item
-let remove s item = (s:Set<ITree> ref) := (!s).Remove item
-let toList s = !s |> Set.toList
-let mutable accumulatorSeed : Set<ITree> ref = ref ([] |> Set.ofList)
-
-let propSet item = item |> justProps |> set
-let faceSet item = item |> justIFaces |> set
-let isPropertySubsetOf s2 s1 = Set.isSubset (s1 |> propSet) (s2 |> propSet)
-let isPropertyMatchedWith s2 s1 = s1 |> isPropertySubsetOf s2 || s2 |> isPropertySubsetOf s1
-let isInterfaceSubsetOf l2 l1 = Set.isSubset (set (l1:ITree).Sub) (set (l2:ITree).Sub)
-    
-let (|InterfaceSubSet|_|) (lhs, rhs) = if lhs |> isInterfaceSubsetOf rhs then Some (lhs, rhs) else None
-let (|InterfaceSuperSet|_|) (lhs, rhs) = if rhs |> isInterfaceSubsetOf lhs then Some (lhs, rhs) else None
-let (|PropertyMatch|_|) (lhs, rhs) = if lhs |> isPropertyMatchedWith rhs then Some (lhs, rhs) else None
-
-let interfacesWithProperties = function | IFace (n, []) -> false | _ -> true
-let face (name, subs) = IFace ({ Name = name }, subs)
-let mergedFace subs = ("IShared", subs) |> face
-let emptyFace = ("IAmEmpty", []) |> face
 
 
-let rec deepMerge (lhs : ITree) (rhs : ITree) : ITree = 
-    let mergedSubFace = lhs.Faces @ rhs.Faces |> List.fold deepMerge emptyFace
+module Interface =         
 
-    [mergedSubFace] @ lhs.Props @ rhs.Props 
-    |> List.filter interfacesWithProperties
-    |> set 
-    |> Set.toList
-    |> mergedFace
+    type IItem = { Name : string; }
 
+    type ITree = 
+        | IFace of IItem * ITree list
+        | IProp of IItem
+        with member x.Item = match x with | IFace(n,sub) -> n | IProp (n) -> n
+             member x.Sub  = match x with | IFace(n,sub) -> sub | IProp _ -> []
+             member x.Props = x.Sub |> List.filter (function | IProp _ -> true | _ -> false)
+             member x.Faces = x.Sub |> List.filter (function | IFace _ -> true | _ -> false)
 
-let merge (interfaces : Set<ITree> ref) (rhs : ITree) (lhs : ITree)  = 
-    match (lhs, rhs) with
-    | InterfaceSubSet _ -> 
-        lhs |> removeFrom interfaces
-        rhs        
-    | InterfaceSuperSet _ -> 
-        lhs
-    | PropertyMatch _ ->
-        lhs |> removeFrom interfaces
-        deepMerge rhs lhs  
-    | _ -> rhs
+    let rec print (tree:ITree) =
+        printfn "interface: %s = %s" tree.Item.Name tree.Item.Name
+        match tree with
+        | IProp _ -> ()
+        | IFace (item, sub) -> for s in sub do print s
 
-let rec interfaceTree (tree : ITree) =
+    [<AutoOpen>]
+    module private Merging = 
 
-    let extractInterface node = 
-        match node with 
-        | IFace _ -> ("IShared", node |> interfaceTree) |> face
-        | IProp _ -> node 
+        let rec propertyTree (Node(token, subTokens):TokenTree) : ITree =
+            match token with 
+                | Header (i,c) | Root (i,c) -> 
+                    let item = { Name  = "I" + token.Title; }
+                    let sub  = [ for i in subTokens do yield propertyTree i ]
+                    IFace(item, sub)     
+                | Property (i,c) | Yaml (i,c) -> 
+                    IProp({ Name = token.Title })
 
-    let mergedWith interfaces node =
-        interfaces 
-        |> withSubtree
-        |> Set.fold (merge interfaces) (node |> extractInterface)
+        let props item = match item with | IProp _ -> true | _ -> false
+        let justProps item = (item:ITree).Sub |> List.filter props 
+        let propSet item = item |> justProps |> set
+        let isPropertySubsetOf s2 s1 = Set.isSubset (s1 |> propSet) (s2 |> propSet)
+        let isPropertyMatchedWith s2 s1 = s1 |> isPropertySubsetOf s2 || s2 |> isPropertySubsetOf s1
+        let isInterfaceSubsetOf l2 l1 = Set.isSubset (set (l1:ITree).Sub) (set (l2:ITree).Sub)
+        let interfacesWithProperties = function | IFace (n, []) -> false | _ -> true
+        let face (name, subs) = IFace ({ Name = name }, subs)
+        let mergedFace subs = ("IShared", subs) |> face
+        let emptyFace = ("IAmEmpty", []) |> face
 
-    let mergeAll (interfaces : Set<ITree> ref) node =
-        node 
-        |> mergedWith interfaces
-        |> addTo interfaces
-        |> ref
+        let (|InterfaceSubSet|_|)   (lhs, rhs) = if lhs |> isInterfaceSubsetOf   rhs then Some (lhs, rhs) else None
+        let (|InterfaceSuperSet|_|) (lhs, rhs) = if rhs |> isInterfaceSubsetOf   lhs then Some (lhs, rhs) else None
+        let (|PropertyMatch|_|)     (lhs, rhs) = if lhs |> isPropertyMatchedWith rhs then Some (lhs, rhs) else None
 
-    match tree.Sub with
-    | [] -> [ emptyFace ]
-    | _ -> 
-           tree.Sub
-           |> List.fold mergeAll accumulatorSeed  
-           |> toList
+        let mutable accumulatorSeed : Set<ITree> ref = ref ([] |> Set.ofList)
+        let withSubtrees s = !(s:Set<ITree> ref) |> Set.filter (fun item -> not (item.Sub.IsEmpty))
+        let removeFrom s item = (s:Set<ITree> ref) := (!s).Remove item
+        let addTo s item = (!(s:Set<ITree> ref)).Add item
+        let toList s = !s |> Set.toList
 
+        let rec deepMerge (lhs : ITree) (rhs : ITree) : ITree = 
+            let mergedSubFace = lhs.Faces @ rhs.Faces |> List.fold deepMerge emptyFace
 
+            [mergedSubFace] @ lhs.Props @ rhs.Props 
+            |> List.filter interfacesWithProperties
+            |> set 
+            |> Set.toList
+            |> mergedFace
+
+        let merge (interfaces : Set<ITree> ref) (rhs : ITree) (lhs : ITree)  = 
+            match (lhs, rhs) with
+            | InterfaceSuperSet _ -> 
+                lhs
+            | InterfaceSubSet _ -> 
+                lhs |> removeFrom interfaces
+                rhs
+            | PropertyMatch _ ->
+                lhs |> removeFrom interfaces
+                deepMerge rhs lhs
+            | _ -> rhs
+
+        let rec mergedTree (tree : ITree) =
+
+            let extractInterface node = 
+                match node with 
+                | IFace _ -> ("IShared", node |> mergedTree) |> face
+                | IProp _ -> node 
+
+            let mergedWith interfaces node =
+                interfaces 
+                |> withSubtrees
+                |> Set.fold (merge interfaces) (node |> extractInterface)
+
+            let mergeAll (interfaces : Set<ITree> ref) node =
+                node 
+                |> mergedWith interfaces
+                |> addTo interfaces
+                |> ref
+
+            match tree.Sub with
+            | [] -> [ emptyFace ]
+            | _ -> 
+                   tree.Sub
+                   |> List.fold mergeAll accumulatorSeed  
+                   |> toList
+
+    let tree (tokens:TokenTree) = tokens |> (propertyTree >> mergedTree)
 
 
 
 printfn "%s" (new System.String('\n', 3))
-let t1t = t1 |> findProps |> interfaceTree
-let t2t = t2 |> findProps |> interfaceTree
-let t3t = t3 |> findProps |> interfaceTree
-let t4t = t4 |> findProps |> interfaceTree
-let t5t = t5 |> findProps |> interfaceTree
+let t1t = t1 |> Interface.tree
+let t2t = t2 |> Interface.tree
+let t3t = t3 |> Interface.tree
+let t4t = t4 |> Interface.tree
+let t5t = t5 |> Interface.tree
 
 
 
@@ -260,7 +261,7 @@ let t5t = t5 |> findProps |> interfaceTree
 
 
 
-module Interface =
+module Interface2 =
 
     let extract (t1 : TokenTree) (t2: TokenTree) =
         
